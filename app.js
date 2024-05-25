d3aedeaad00952d0870f6ed821d4e91db0079d1b
debrajsaha252 <165931790+debrajsaha252@users.noreply.github.com>
2024-05-25 13:58:08 +0530

    Login,Signup&Dashboard_website_(express,ejs,mongodb)

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set(express.static('./public'))
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/PROJECT_LOG_SIGN').then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
+// Routes
+app.use('/', authRoutes);
+
+
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
